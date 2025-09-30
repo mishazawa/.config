1c80c0ba03be6b51337de4bd3f1f214ac812f37c
mishazawa <macintoshkrug@gmail.com>
2025-09-30 16:12:10 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,39 @@
-function find_window_under_cursor () 
+local log = hs.logger.new('Missha', 'info')
+local FLOATING_SPACE = 2
+
+local function getSpaceNames()
+  local spacesByDisplay = hs.spaces.allSpaces()
+  local result = {}
+  for display, spaces in pairs(spacesByDisplay) do
+    for i, sid in ipairs(spaces) do
+      result[sid] = "Desktop " .. i
+    end
+  end
+  return result
+end
+
+function findWindowUnderCursor () 
   local position = hs.geometry.new(hs.mouse.absolutePosition())
   local screen = hs.mouse.getCurrentScreen()
   return hs.fnutils.find(hs.window.orderedWindows(),
     function(w) return screen == w:screen() and position:inside(w:frame()) end)
 end
 
-function reload_auto_raise()
-  local app = hs.application.get("AutoRaise")
+function reloadAutoRaise()
+  local appName = "AutoRaise"
+  local appPath = "/Applications/AutoRaise.app"
+  local appArgs = [[--args -pollMillis 50 -delay 1 -scale 2.5 -altTaskSwitcher false -ignoreSpaceChanged false -disableKey control -mouseDelta 100.0]]
+
+  local app = hs.application.get(appName)
   if app then
     app:kill()
-    hs.timer.doAfter(0.5, function()  -- small delay to ensure quit
-      hs.execute([[open -n /Applications/AutoRaise.app --args -pollMillis 50 -delay 1 -scale 2.5 -altTaskSwitcher false -ignoreSpaceChanged false -disableKey control -mouseDelta 100.0]], true)
-    end)
-  else
-    hs.execute([[open -n /Applications/AutoRaise.app --args -pollMillis 50 -delay 1 -scale 2.5 -altTaskSwitcher false -ignoreSpaceChanged false -disableKey control -mouseDelta 100.0]], true)
   end
+
+  hs.timer.doAfter(0.5, function()
+    local cmd = string.format("open -n %s %s", appPath, appArgs)
+    hs.execute(cmd, true)
+    log.i("AutoRaise reloaded.")
+  end)
 end
 
 
@@ -22,7 +41,7 @@ local cancelCmdClick = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }
   local mods = hs.eventtap.checkKeyboardModifiers()
   
   if mods["alt"] then
-    local window = find_window_under_cursor()
+    local window = findWindowUnderCursor()
     -- if no window under cursor then skip
     if not window then return end
     -- if the same window then skip
@@ -31,6 +50,24 @@ local cancelCmdClick = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }
     window:focus()
    end
 end)
+
+local spaceNames = getSpaceNames()
+
+spacesWatcher = hs.spaces.watcher.new(function()
+    local spaceID = hs.spaces.focusedSpace()
+    local name = spaceNames[spaceID] or ("Space " .. tostring(spaceID))
+
+    if name == "Desktop 2" then
+        hs.execute("aerospace enable off", true)
+        log.i("Aerospace disabled on " .. name)
+    else
+        hs.execute("aerospace enable on", true)
+        log.i("Aerospace enabled on " .. name)
+    end
+end)
+
+
+
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   cancelCmdClick:stop()
@@ -58,6 +95,13 @@ hs.hotkey.bind({"shift", "alt"}, "delete", function()
 end)
 
 
-reload_auto_raise()
-cancelCmdClick:start()
-hs.alert.show("Config loaded")
+
+
+local function initConfig()
+  reloadAutoRaise()
+  cancelCmdClick:start()
+  hs.alert.show("Config loaded")
+end
+
+-- Run startup tasks
+initConfig()
