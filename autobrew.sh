2b0ca8548d98f0767a1cc654f3cc5c74c1cdea08
mishazawa <macintoshkrug@gmail.com>
2025-10-17 04:53:13 +0000

    Create autobrew.sh

---
+++ autobrew.sh
@@ -0,0 +1,22 @@
+brew install  git \
+              qbittorrent \
+              vlc \
+              yt-dlp \
+              ffmpeg \
+              kid3 \
+              openmtp \
+              nvm \
+              font-fira-code \
+              the-unarchiver\
+              sevenzip \
+              alt-tab \
+              vscodium \
+              openjdk@17
+
+brew install --cask karabiner-elements \
+                    hammerspoon \
+                    nikitabobko/tap/aerospace \
+                    dimentium/autoraise/autoraiseapp \
+                    miniconda 
+
+brew install librewolf --no-quarantine 
